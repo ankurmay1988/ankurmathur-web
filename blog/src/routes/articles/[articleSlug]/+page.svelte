4974a58fe92ea7293688c7de95a20f75a048e123
Ankur Mathur <ankurakaet@gmail.com>
2026-05-04 00:58:12 +0530

    test commit

---
+++ blog/src/routes/articles/[articleSlug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageProps } from "./$types";
+
+    let { params }: PageProps = $props();
+
+    let slug = $derived(params.articleSlug);
+</script>
+
+<h1>Article: {slug}</h1>
